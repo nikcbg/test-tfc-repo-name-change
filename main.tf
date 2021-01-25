55e7068ec3dc9383eb3eb338df9e3aa6af4629b5
nikcbg <nikcbg@yahoo.com>
2021-01-25 15:26:37 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "example" {
+  count = 5
+
+  triggers = {
+    static_trigger = "Terraform"
+  }
+provisioner "local-exec" {
+    command = "echo example${count.index}"
+  }
+  
+}
